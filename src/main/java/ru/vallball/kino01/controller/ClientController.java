557e5dc057c52a628b3056cb21372196174d93d0
vall-ball <vall-ball@yandex.ru>
2019-10-23 13:32:20 +0700

    The first commit 23.10.2019

--- src/main/java/ru/vallball/kino01/controller/ClientController.java
+++ src/main/java/ru/vallball/kino01/controller/ClientController.java
@@ -32,23 +32,23 @@ import ru.vallball.kino01.service.UserService;
 @Controller
 @RequestMapping(value = { "/forClient" })
 public class ClientController {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Autowired
 	FilmService filmService;
-	
+
 	@Autowired
 	SessionService sessionService;
-	
+
 	@Autowired
 	PlaceService placeService;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ClientController.class);
 
 	@GetMapping
-	public String clientPage(Model model, HttpServletRequest req, 
+	public String clientPage(Model model, HttpServletRequest req,
 			@PageableDefault(page = 0, size = 10) Pageable pageable) {
 		User user = (User) userService.loadUserByUsername(req.getRemoteUser());
 		model.addAttribute("user", user);
@@ -61,16 +61,13 @@ public class ClientController {
 		}
 		return "forClient";
 	}
-	
+
 	@GetMapping("/sessionOfFilm/{id}")
 	public String sessionOfFilm(@PathVariable("id") Long id, HttpServletRequest req, Model model,
 			@PageableDefault(page = 0, size = 10) Pageable pageable) {
 		Film film = filmService.findFilmById(id);
-		Page<Session> page = sessionService.findAllByFilm(pageable,film);
+		Page<Session> page = sessionService.findAllByFilm(pageable, film);
 		model.addAttribute("page", page);
-		
-		//List<Session> sessions = sessionService.findAllByFilm(film);
-		//model.addAttribute("sessions", sessions);
 		int totalPages = page.getTotalPages();
 		if (totalPages > 0) {
 			List<Integer> pageNumbers = IntStream.rangeClosed(0, totalPages - 1).boxed().collect(Collectors.toList());
@@ -79,9 +76,9 @@ public class ClientController {
 		model.addAttribute("film", film);
 		User user = (User) userService.loadUserByUsername(req.getRemoteUser());
 		model.addAttribute("user", user);
-		return "sessionsOfFilm";		
+		return "sessionsOfFilm";
 	}
-	
+
 	@GetMapping("/choosePlace/{id}")
 	public String chooseSession(@PathVariable("id") Long id, HttpServletRequest req, Model model) {
 		Session sess = sessionService.findSessionById(id);
@@ -89,27 +86,22 @@ public class ClientController {
 		model.addAttribute("sess", sess);
 		model.addAttribute("user", user);
 		List<Place> places = placeService.findAllBySession(sess);
-		for (Place place : places) {
-			logger.info("Места " + place);
-		}
 		List<Row> rows = new ArrayList<>();
 		for (int i = 1; i <= 5; i++) {
 			rows.add(new Row(i));
 		}
 		for (Row row : rows) {
-			logger.info("Ряд " + row.getRow());
 			for (Place place : places) {
 				if (place.getLine() == row.getRow()) {
-					logger.info("Место " + place.getNumber());
 					row.getPlaces().add(place);
 				}
-				
+
 			}
 		}
-	
-		model.addAttribute(rows);
+
+		model.addAttribute("rows", rows);
 		return "choosePlace";
-		
+
 	}
 
 }
